7ad8523e22ab6bdc977b7e65ca010240187e2480
Anthony Borton <anthonyborton@github.com>
2023-04-20 09:39:37 +1000

    Sample code

--- utils.py
+++ utils.py
@@ -1,9 +1,18 @@
 import re
-e_regex = r,/v[\w\.\+\-l+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$' p_regex = r,/v(\+)?l?\d{9,15}$'
-s_regex = r,/v(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
-def checkvalidftext, regex): if re.searchfregex, text):
-return True else:
-return False
-if 	name	 == '	main	':
-print(("valid", "invalid")[checkvalid('',e_regex))) print(("valid", "invalid")[checkvalidf'',p_regex))) print(("valid", "invalid")[checkvalid(1',s_regex)])
+
+e_regex = r'^[\w\.\+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$'
+p_regex = r'^(\+)?1?\d{9,15}$'
+s_regex = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
+
+def checkvalid(text, regex):
+    if re.search(regex, text):
+        return True
+    else:
+        return False
+
+if __name__	== '__main__':
+    print(("valid", "invalid")[checkvalid('',e_regex)])
+    print(("valid", "invalid")[checkvalid('',p_regex)])
+    print(("valid", "invalid")[checkvalid('',s_regex)])
+
 
