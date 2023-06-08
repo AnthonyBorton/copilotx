fefef48a739e2e4be8d219e612069106711c2576
Damian Brady <damovisa@github.com>
2023-06-08 20:49:25 +1000

    More obfuscation examples

---
+++ obfuscated.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class dnduxb
+{
+double ndndn;
+void _bxnuu(double _nneop)
+{
+ndndn= (22*_nneop*_nneop)/7;
+}
+}
+class _jnsnek extends dnduxb
+{
+public static void main(String args[])
+{
+Scanner _jp= new Scanner();
+System.out.println("nejshzyyhejeisns");
+double _nbrudu= _jp.nextDouble();
+_jnsnek jdjdj=new _jnsnek();
+jdjdj._bxnuu(_nbrudu);
+System.out.println("cjxhxyhhbebj " + jdjdj.ndndn);
+}
+}
